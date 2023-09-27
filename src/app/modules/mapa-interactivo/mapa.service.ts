e83fb2c30254ce79fb0c597c60dfc2c629f9c374
AaronMartinezMtz <56774670+AaronMartinezMtz@users.noreply.github.com>
2023-09-27 18:49:35 -0500

    Lugares Terminados

--- src/app/modules/mapa-interactivo/mapa.service.ts
+++ src/app/modules/mapa-interactivo/mapa.service.ts
@@ -32,4 +32,16 @@ export class MapaService {
   }
 
 
+  getPosition(): Promise<any> {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resp => {
+                resolve({lng: resp.coords.longitude, lat: resp.coords.latitude});
+            },
+            err => {
+                reject(err);
+          });
+    });
+}
+
+
 }
